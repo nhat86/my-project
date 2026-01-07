b0d6f0495065cc2ad94057e794ea934cbfe6dde0
nhatvo <vtmnhat@gmail.com>
2026-01-07 15:34:38 +0100

    prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,22 +1,18 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // 👈 Quan trọng để tránh lỗi TLS self-signed
-  },
-});
-
-const adapter = new PrismaPg(pool);
-
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
